ce914da3f734b111a0db59717c4d928f852352cc
Samuel Sutch <sam@sutch.net>
2015-11-10 14:58:31 -0700

    some stuff

--- RIDE/ViewController.swift
+++ RIDE/ViewController.swift
@@ -6,10 +6,22 @@
 //  Copyright © 2015 Samuel Sutch. All rights reserved.
 //
 
+import Foundation
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    var opQ: NSOperationQueue
+    var ca: CMAltimeter?
+    @IBOutlet var lab: UILabel?
+    @IBOutlet var lab2: UILabel?
+    
+    required init?(coder aDecoder: NSCoder) {
+        opQ = NSOperationQueue()
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if CMAltimeter.isRelativeAltitudeAvailable() {
+            NSLog("relative altimeter is available")
+            
+            ca = CMAltimeter()
+            ca!.startRelativeAltitudeUpdatesToQueue(opQ, withHandler: { (altData, err) -> Void in
+                NSLog("altData: \(altData)")
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    if let altData = altData {
+                        self.lab?.text = "\(altData.relativeAltitude.description)"
+                        self.lab2?.text = "\(altData.pressure.description)"
+                    }
+                    
+                })
+            })
+            
+        }
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+        ca?.stopRelativeAltitudeUpdates()
+    }
 
 }
 
