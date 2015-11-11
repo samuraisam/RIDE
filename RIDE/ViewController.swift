23f62c0f92d7b636587045e6a4071998dee290ee
Samuel Sutch <sam@sutch.net>
2015-11-10 20:14:07 -0700

    y effects

--- RIDE/ViewController.swift
+++ RIDE/ViewController.swift
@@ -42,43 +42,61 @@ class ViewController: UIViewController {
         let xa = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
         xa.maximumRelativeValue = NSNumber(float: -20.0)
         xa.minimumRelativeValue = NSNumber(float: 20.0)
+        let ya = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya.maximumRelativeValue = NSNumber(float: 0)
+        ya.minimumRelativeValue = NSNumber(float: -15.0)
         bgMotionEffect = UIMotionEffectGroup()
-        bgMotionEffect?.motionEffects = [xa]
+        bgMotionEffect?.motionEffects = [xa, ya]
         self.ridebg?.addMotionEffect(bgMotionEffect!)
 
         let xa1 = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
         xa1.minimumRelativeValue = NSNumber(float: -15.0)
         xa1.maximumRelativeValue = NSNumber(float: 15.0)
+        let ya1 = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya1.minimumRelativeValue = NSNumber(float: 0)
+        ya1.maximumRelativeValue = NSNumber(float: 15)
         fgMotionEffect = UIMotionEffectGroup()
-        fgMotionEffect?.motionEffects = [xa1]
+        fgMotionEffect?.motionEffects = [xa1, ya1]
         self.mtnfront?.addMotionEffect(fgMotionEffect!)
         
         let xa2 = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
-        xa2.minimumRelativeValue = NSNumber(float: -32.0)
-        xa2.maximumRelativeValue = NSNumber(float: 32.0)
+        xa2.minimumRelativeValue = NSNumber(float: -20)
+        xa2.maximumRelativeValue = NSNumber(float: 20.0)
+        let ya2 = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya2.minimumRelativeValue = NSNumber(float: -15)
+        ya2.maximumRelativeValue = NSNumber(float: 15)
         leftbgMotionEffect = UIMotionEffectGroup()
-        leftbgMotionEffect?.motionEffects = [xa2]
+        leftbgMotionEffect?.motionEffects = [xa2, ya2]
         self.mtnleftbg?.addMotionEffect(leftbgMotionEffect!)
         
         let xa3 = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
-        xa3.minimumRelativeValue = NSNumber(float: -39.0)
-        xa3.maximumRelativeValue = NSNumber(float: 29.0)
+        xa3.minimumRelativeValue = NSNumber(float: -40)
+        xa3.maximumRelativeValue = NSNumber(float: 15.0)
+        let ya3 = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya3.minimumRelativeValue = NSNumber(float: -20)
+        ya3.maximumRelativeValue = NSNumber(float: 20)
         leftmdMotionEffect = UIMotionEffectGroup()
-        leftmdMotionEffect?.motionEffects = [xa3]
+        leftmdMotionEffect?.motionEffects = [xa3, ya3]
         self.mtnleftmid?.addMotionEffect(leftmdMotionEffect!)
         
         let xa4 = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
         xa4.minimumRelativeValue = NSNumber(float: -25.0)
-        xa4.maximumRelativeValue = NSNumber(float: 25.0)
+        xa4.maximumRelativeValue = NSNumber(float: 30)
+        let ya4 = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya4.minimumRelativeValue = NSNumber(float: -30)
+        ya4.maximumRelativeValue = NSNumber(float: 10)
         rightbgMotionEffect = UIMotionEffectGroup()
-        rightbgMotionEffect?.motionEffects = [xa4]
+        rightbgMotionEffect?.motionEffects = [xa4, ya4]
         self.mtnrightbg?.addMotionEffect(rightbgMotionEffect!)
         
         let xa5 = UIInterpolatingMotionEffect(keyPath: "center.x", type: .TiltAlongHorizontalAxis)
-        xa5.minimumRelativeValue = NSNumber(float: -23.0)
-        xa5.maximumRelativeValue = NSNumber(float: 23.0)
+        xa5.minimumRelativeValue = NSNumber(float: -40.0)
+        xa5.maximumRelativeValue = NSNumber(float: 40.0)
+        let ya5 = UIInterpolatingMotionEffect(keyPath: "center.y", type: .TiltAlongVerticalAxis)
+        ya5.minimumRelativeValue = NSNumber(float: 30)
+        ya5.maximumRelativeValue = NSNumber(float: 5)
         rightmidMotionEffect = UIMotionEffectGroup()
-        rightmidMotionEffect?.motionEffects = [xa5]
+        rightmidMotionEffect?.motionEffects = [xa5, ya5]
         self.mtnrightmid?.addMotionEffect(rightmidMotionEffect!)
     }
 
